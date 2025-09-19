ab43c3ea86781a8995850de9f38dd71e6b4066bb
Piyush <rathorepiyush0000@gmail.com>
2025-09-20 00:15:08 +0530

    ho gya bc

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,13 +1,24 @@
 import { AuthProvider } from './Hooks/AuthContext'; // Adjust path if needed
 import './globals.css';
 
+export const metadata = {
+  title: "TBS",
+  description: "The Book Shelves",
+  icons: {
+    icon: [
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: "/site.webmanifest",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
